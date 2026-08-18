52f9e7c284406659b83fd65e89104ad300dcc9aa
SOFTMAN20 <elonsoftware2020@gmail.com>
2026-08-18 10:59:26 +0300

    hello world

--- src/components/IndustriesSection.tsx
+++ src/components/IndustriesSection.tsx
@@ -91,7 +91,7 @@ const IndustriesSection = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
           {industries.map((industry, index) => (
             <IndustryCard
               key={index}
